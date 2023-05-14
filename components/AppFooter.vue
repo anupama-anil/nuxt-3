f9575733f84d3255928d299adb0d3952f4b289c8
Anupama Anilkumar <anupama@techgentsia.com>
2023-05-14 18:14:45 +0530

    feat: components added

---
+++ components/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="footer">
+    Thank you
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+/* Header/Logo Title */
+.footer {
+  padding: 20px;
+  text-align: center;
+  background: #da2357;
+  color: white;
+  font-size: 30px;
+}
+</style>
